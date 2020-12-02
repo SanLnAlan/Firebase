89b4960d5a6fa1eca8af0330b60e4f39b3b4f1cf
Alan <sandoval.leon.alan@gmail.com>
2020-12-01 22:22:49 -0600

    remove y create agregados con fallas

--- VueJS/app.js
+++ VueJS/app.js
@@ -10,19 +10,55 @@ var db = firebase.firestore();
 let usuarios= [];
 db.collection("usuarios").onSnapshot((querySnapshot) => {
   querySnapshot.forEach((doc) => {
-			console.log(doc.data());
-			usuarios.push(doc.data(), doc.id);
+	let datos = new Object();
+	datos.id = doc.id;
+	datos.data = doc.data();
+	console.log(datos);
+	usuarios.push(datos);
   });
 });
 
-console.log("usuarios: ");
-console.log(usuarios);
 
 const app = new Vue({
 	el: '#app',
 	data: {
 		titulo: 'Hola mundo',
 		titulo_tabla: 'Registro de materia prima',
-		materias: usuarios		
-	}
+		materias: usuarios,
+		n_nombre: '',		
+		n_apellido: '',		
+		n_fecha: '',
+		showNuevo: true,
+	},
+	methods: {
+		eliminar(id){
+			alert(id);
+			db.collection("usuarios").doc(id).delete().then(function() {
+				console.log("Document successfully deleted!");
+			}).catch(function(error) {
+				console.error("Error removing document: ", error);
+			});
+		},
+		guardar(){
+
+			if(this.n_nombre == '' || this.n_apellido == '' || this.n_fecha == ''){
+				alert("Favor de llenar todos los campos");
+			}else{
+				db.collection("usuarios").add({
+					nombre: this.n_nombre,
+					apellido: this.n_apellido,
+					fecha_nacimiento: this.n_fecha
+			})
+			.then(function(docRef) {
+					console.log("Document written with ID: ", docRef.id);
+					this.n_nombre = "";
+					this.n_apellido = "";
+					this.n_fecha= "";
+			})
+			.catch(function(error) {
+					console.error("Error adding document: ", error);
+			});
+			}
+		},
+	},
 });
